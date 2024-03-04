f4edb0a8833dbdcd8cd6b63ad12e3297e88cb6f8
hitsutakarin77@gmail.com <hitsutakarin77@gmail.com>
2024-03-04 21:44:32 +0700

    aaa kasian aaa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter, usePathname } from "next/navigation";
 
